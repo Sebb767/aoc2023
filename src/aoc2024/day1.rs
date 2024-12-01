6cf9e73cfc6fdb2d895cd9d7e8c39b0b0f612211
Sebastian Kaim <sebastian.kaim@kaim-consulting.com>
2024-12-02 00:32:24 +0100

    2024 day 1-2

--- src/aoc2024/day1.rs
+++ src/aoc2024/day1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::hash::Hash;
 use crate::tools::get_input_or_panic;
 
 pub fn day1() {
@@ -5,13 +7,13 @@ pub fn day1() {
     day1_2();
 }
 
-fn day1_1() {
+fn read_input() -> (Vec<u64>, Vec<u64>) {
     let input = get_input_or_panic("1-1", 2024);
-    let lines : Vec<&str> = input.lines().collect();
+    let lines: Vec<&str> = input.lines().collect();
     let count = lines.len();
 
-    let mut left : Vec<u64> = Vec::with_capacity(count);
-    let mut right : Vec<u64> = Vec::with_capacity(count);
+    let mut left: Vec<u64> = Vec::with_capacity(count);
+    let mut right: Vec<u64> = Vec::with_capacity(count);
 
     for line in lines.into_iter() {
         let numbers = line.split_whitespace().collect::<Vec<&str>>();
@@ -25,6 +27,12 @@ fn day1_1() {
     right.sort();
     assert_eq!(left.len(), right.len());
 
+    return (left, right);
+}
+
+fn day1_1() {
+    let (left, right) = read_input();
+
     let mut ileft = left.into_iter();
     let mut iright = right.into_iter();
     let mut result = 0u64;
@@ -36,6 +44,33 @@ fn day1_1() {
     println!("Sum of differences of numbers: {result}");
 }
 
+fn list_to_occurrence_map<T>(inp : &Vec<T>) -> HashMap<T, u64> where T : Hash + Eq + Copy {
+    let mut map: HashMap<T, u64> = HashMap::new();
+
+    for num in inp.into_iter() {
+        if let Some(kref) = map.get_mut(&num) {
+            *kref += 1;
+        }
+        else {
+            map.insert(*num, 1);
+        }
+    }
+
+    map
+}
+
 fn day1_2() {
-    let _input = get_input_or_panic("1-1", 2024);
+    let (left, right) = read_input();
+    let lmap = list_to_occurrence_map(&left);
+    let rmap = list_to_occurrence_map(&right);
+
+    let mut result = 0u64;
+    for key in lmap.keys() {
+        let amount_right = *lmap.get(key).unwrap();
+        if let Some(amount_left) = rmap.get(key) {
+            result += *key * amount_right * *amount_left;
+        }
+    }
+
+    println!("Difference score: {result}");
 }
